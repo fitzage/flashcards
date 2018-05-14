48e0fa94c195569374c0ff87683db5cc93373715
Matthew Fitzsimmons <matt@fitzage.com>
2018-05-14 12:56:04 -0700

    feat: added status bar

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Text, View, StatusBar } from 'react-native'
+import { Constants } from 'expo'
 import DeckList from './components/DeckList'
 
 export default class App extends React.Component {
   render() {
     return (
       <View>
+        <View style={{ height: Constants.statusBarHeight }}>
+          <StatusBar translucent />
+        </View>
         <DeckList />
       </View>
     )
